c4ccfb1ba246180c8bf3925561972455790064e0
Martin Braun <martinbraun123@aol.com>
2016-05-12 15:12:29 +0200

    Wip. implemented parallelism. cleanup of the code should be next

--- parr.hs
+++ parr.hs
@@ -1,8 +1,11 @@
+import Control.Monad.Par (runPar, spawnP, get)
+import Control.Parallel.Strategies
+
 -- basic arrows
 class Arrow a where
   arr :: (b -> c) -> a b c
   (>>>) :: a b c -> a c d -> a b d
-  first :: a b c -> a (b,d) (c,d)
+  --first :: a b c -> a (b,d) (c,d)
 
 instance Arrow (->) where
   arr = id
@@ -41,106 +44,105 @@ instance Arrow (ParKleisli) where
   arr f = P $ return . f
   P f >>> P g = P (\b -> f b >>= g)
 
-
-example1 =
-    let f = P $ \x -> PR $ x+1
-        g = P $ \x -> PR $ x*2
-    in f >>> g
--- conditional, f before g
-
-example2 =
-      let f = (+1)
-          g = (*2)
-      in f <|||> g <&&&> (+)
-
 -- idea lazily copied from:
 -- https://gist.github.com/morae/8494016
 
+fsthalf :: [a] -> [a]
+fsthalf xs = take (length xs `div` 2) xs
+
+sndhalf :: [a] -> [a]
+sndhalf xs = drop (length xs `div` 2) xs
+
+merge :: (a -> a -> Ordering) -> [a] -> [a] -> [a]
+merge _ xs [] = xs
+merge _ [] ys = ys
+merge fn (x:xs) (y:ys)
+      | fn x y == LT = x:merge fn xs (y:ys)
+      | fn x y == EQ = x:merge fn xs (y:ys)
+      | otherwise = y:merge fn (x:xs) ys
+
+-- this is our multithreaded function
+-- it spawns 2 threads that do the mergesort
+mergesortPar :: (NFData a) => (a -> a -> Ordering) -> [a] -> [a]
+mergesortPar fn xs
+  | length xs <= 1 = xs
+  -- we get multithreading for zero readability cost here
+  -- first we split the sorting process into two different processes <|||>
+  -- and then we merge them with <&&&>
+  | otherwise = (mergesort fn <|||> mergesort fn <&&&> merge fn) (fsthalf xs, sndhalf xs)
+
 mergesort :: (a -> a -> Ordering) -> [a] -> [a]
 mergesort fn xs
-    | len == 1 = xs
-    -- we get multithreading for zero readability cost here
-    -- first we split the sorting process into two different processes
-    -- and then we merge them with the merge fn
-    -- this is probably not the best performance as we aggressively
-    -- multithread this here and probably end up having to wait most of the time
-    --
-    -- a good idea would be to have a helper function that does't spawn any
-    -- new threaded computations
-    | otherwise = (mergesort fn <|||> mergesort fn <&&&> merge fn) (fsthalf xs, sndhalf xs)
-       where len = length xs
-             fsthalf :: [a] -> [a]
-             fsthalf xs = take (length xs `div` 2) xs
+    | length xs <= 1 = xs
+    | otherwise = merge fn (mergesort fn (fsthalf xs)) (mergesort fn (sndhalf xs))
 
-             sndhalf :: [a] -> [a]
-             sndhalf xs = drop (length xs `div` 2) xs
+parMapPR :: ParKleisli a b -> [a] -> ParRes [b]
+parMapPR (P f) = sequence . fmap f
 
-             merge :: (a -> a -> Ordering) -> [a] -> [a] -> [a]
-             merge fn xs [] = xs
-             merge fn [] ys = ys
-             merge fn (x:xs) (y:ys)
-                   | (fn x y) == LT = x:(merge fn xs (y:ys))
-                   | (fn x y) == EQ = x:(merge fn xs (y:ys))
-                   | otherwise = y:(merge fn (x:xs) ys)
-
--- f and g get evaluated in parallel and their results are then
--- merged into a single result with (+)
--- this can be particularly useful for two long running
--- tasks f and g that are independent of each others' results
---
--- by using this style (and by having the ParKleisli arrow)
--- we can easily represent _(simple) dependency graphs of computations
---
 -- using split <|||> and merge <&&&> basically every computation
 -- can be parallelized but if the single tasks are already computed
 -- quite fast, the overhead for splitting and merging
 -- is probably too high (regarding both computationally and
 -- code length/complexity due to the types ending up being something like
--- (,,,) (,,,) if merges dont happen)
-
-parMapPR :: ParKleisli a b -> [a] -> ParRes [b]
-parMapPR (P f) = sequence . fmap f
-
-parMap :: (a -> b) -> [a] -> [b]
-parMap = map
+-- ((,),(,)), etc. if merges dont happen)
 
 -- this could be broken down for arrows so we can use this kind
 -- of parallelism with normal functions
 
 class (Arrow arr) => ParallelSplit arr where
-    (<||>) :: arr a b -> arr a c -> arr a (b, c)
+    (<||>) :: (NFData b, NFData c) => arr a b -> arr a c -> arr a (b, c)
     (<&&>) :: arr a (b, c) -> (b -> c -> d) -> arr a d
 
-    (<|||>) :: arr a b -> arr c d -> arr (a, c) (b, d)
+    (<|||>) :: (NFData b, NFData d) => arr a b -> arr c d -> arr (a, c) (b, d)
     (<&&&>) :: arr (a, c) (b, d) -> (b -> d -> e) -> arr (a, c) e
 
-    (<||||>) :: arr a b -> arr [a] [b]
-    (<&&&&>) :: arr [a] [b] -> (b -> b -> b) -> arr [a] b
+    liftToParMap :: (NFData b) => arr a b -> arr [a] [b]
+    reduce :: arr [a] [b] -> (b -> b -> b) -> arr [a] b
+
+-- TODO: remove duplicated code
 
 instance ParallelSplit ParKleisli where
     -- do this with the par monad so we can have this type in parallel
-    (<||>) f g = P $ \a -> PR (evalKleisli f a, evalKleisli g a)
+    (<||>) f g = P $ \a -> PR $ runPar $ do y1 <- spawnP (evalKleisli f a)
+                                            y2 <- spawnP (evalKleisli g a)
+                                            b  <- get y1
+                                            c  <- get y2
+                                            return (b, c)
     (<&&>) (P f) mergefn = P $ \a -> let (PR bc) = f a
                                      in PR $ uncurry mergefn bc
     -- do this with the par monad so we can have this type in parallel
-    (<|||>) f g = P $ \(a, b) -> PR (evalKleisli f a, evalKleisli g b)
+    (<|||>) f g = P $ \(a, c) -> PR $ runPar $ do y1 <- spawnP (evalKleisli f a)
+                                                  y2 <- spawnP (evalKleisli g c)
+                                                  b  <- get y1
+                                                  d  <- get y2
+                                                  return (b, d)
     (<&&&>) (P f) mergefn = P $ \ac -> let (PR bd) = f ac
                                        in PR $ uncurry mergefn bd
-    (<||||>) f = P $ \as -> parMapPR f as
-    (<&&&&>) (P f) mergefn = P $ \as -> let (PR bs) = f as
-                                        in PR $ foldr1 mergefn bs
+    -- foldr1 is probably not the best choice for this?
+    liftToParMap f = P $ \as -> parMapPR f as
+    reduce (P f) mergefn = P $ \as -> let (PR bs) = f as
+                                      in PR $ foldr1 mergefn bs
 
 instance ParallelSplit (->) where
-    (<||>) f g = \a -> (f a, g a)
+    (<||>) f g = \a -> runPar $ do y1 <- spawnP (f a)
+                                   y2 <- spawnP (g a)
+                                   b  <- get y1
+                                   c  <- get y2
+                                   return (b, c)
     (<&&>) f mergefn = \a -> let bc = f a
                              in uncurry mergefn bc
   -- do this with the par monad so we can have this type in parallel
-    (<|||>) f g = \(a, b) -> (f a, g b)
+    (<|||>) f g = \(a, c) -> runPar $ do y1 <- spawnP (f a)
+                                         y2 <- spawnP (g c)
+                                         b  <- get y1
+                                         d  <- get y2
+                                         return (b, d)
     (<&&&>) f mergefn = \ac -> let bd = f ac
                                in uncurry mergefn bd
-    (<||||>) f = \as -> parMap f as
-    (<&&&&>) f mergefn = \as -> let bs = f as
-                                in foldr1 mergefn bs
+    -- foldr1 is probably not the best choice for this?
+    liftToParMap f = \as -> parMap rdeepseq f as
+    reduce f mergefn = \as -> let bs = f as
+                              in foldr1 mergefn bs
 
 
 unwrapKleisli :: ParKleisli a b -> (a -> ParRes b)
@@ -153,4 +155,13 @@ evalKleisli :: ParKleisli a b -> a -> b
 evalKleisli fn a = unwrapParRes $ unwrapKleisli fn a
 
 main = do
-    print ""
+    print $ mergesortPar order stuff
+
+stuff :: [Integer]
+stuff = [1..999999]
+
+
+order x y
+    | x > y = GT
+    | x < y = LT
+    | x == y = EQ
