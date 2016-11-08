e4ad4b33a45d0492883a773d48bdbaf83469c158
s4ke <martinbraun123@aol.com>
2016-11-08 14:31:40 +0100

    reworked project structure so that the user doesnt need all parallel libraries as a dependency if he really just needs one

--- executable/Main.hs
+++
@@ -1,6 +0,0 @@
--- It is generally a good idea to keep all your business logic in your library
--- and only use it in the executable. Doing so allows others to use what you
--- wrote in their libraries.
-
-main :: IO ()
-main = print "nothing"
