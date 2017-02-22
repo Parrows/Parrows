4f26752b30b01a829ac5c4b06f7b7c08d319c4e1
Martin Braun <martinbraun123@aol.com>
2017-02-22 19:08:16 +0100

    HdpH version compiles now (with custom version of HdpH)

--- HdpH/library/Parrows/HdpH.hs
+++ HdpH/library/Parrows/HdpH.hs
@@ -34,8 +34,8 @@ import Control.Applicative
 import Control.Monad
 
 import Control.DeepSeq
+
 import Control.Parallel.HdpH
-import Control.Parallel.HdpH.Closure
 import Control.Parallel.HdpH.Strategies
 
 import Data.Binary(encode, decode)
@@ -57,13 +57,17 @@ class HdpHConf conf where
 instance HdpHConf RTSConf where
     rtsConf = id
 
+type FunctionTypeToken a b = [a -> b]
+
 -- class to extract the Strategy out of the conf
 class HdpHStrategy conf b where
-    strategy :: conf -> Closure (Strategy (Closure b))
+    strategy :: conf -> Serialized (Strategy (Serialized b))
+
 
 -- default implementation for basic RTSConf, uses forceCC as the strategy
-instance (ForceCC b) => HdpHStrategy RTSConf b where
+instance (NFData b) => HdpHStrategy RTSConf b where
     strategy _ = forceCC
+
 
 -- | default buffer size used by trySerialize
 defaultBufSize :: Int
@@ -75,32 +79,17 @@ class SerializeConf conf where
 
 instance SerializeConf RTSConf
 
-instance ToClosure ByteString where
--- FIXME: actual implementation
 
--- FIXME: this does not run in parallel, yet
--- we probably need to change this so we don't rely on the input strategy
--- but instead make the user specify a Closure (Strategy (Closure ByteString))
--- for this we have to change the strategy function in the HdpHStrategy typeclass
--- so we can pass it the proper type to deserialize the ByteString from
-parClosureListSerialized :: (Typeable b, ToClosure b, SerializeConf conf) => conf -> [a -> b] -> Closure (Strategy (Closure b)) -> Strategy [Closure (ByteString)]
--- ignore the [a -> b] argument. this is just here to we have the proper type
-parClosureListSerialized conf _ clo_strat xs = mapM (sparkClosure clo_strat) deserialized >>=
-                              mapM get >>=
-                              mapM (return . toClosure . encode . unsafePerformIO . (flip trySerializeWith $ serializeBufSize conf) . unClosure)
-                              where
-                                deserialized = map (toClosure . unsafePerformIO . deserialize . decode . unClosure) xs
-
-instance (SerializeConf conf, HdpHConf conf, HdpHStrategy conf b, Typeable b, ToClosure b) => ArrowParallel (->) a b conf where
+instance (SerializeConf conf, HdpHConf conf, HdpHStrategy conf b, Typeable b, NFData b) => ArrowParallel (->) a b conf where
      parEvalN conf fs as = fromJust' $ unsafePerformIO $ runParIO (rtsConf conf) $
                  -- apply the functions to the inputs, serialize the not yet computed state so that we can send this over the network
                  -- and put it into a [Closure (ByteString)], this is then fully evaluated with parClosureListSerialized
                  -- but stays a [Closure (ByteString)]
-                 do clo_bs_evaluated <- map (toClosure . encode . unsafePerformIO . (flip trySerializeWith $ serializeBufSize conf)) (zipWith ($) fs as)
+                 do clo_bs_evaluated <- map (unsafePerformIO . (flip trySerializeWith $ serializeBufSize conf)) (zipWith ($) fs as)
                                         `using`
-                                        parClosureListSerialized conf fs (strategy conf)
+                                        parClosureList (strategy conf)
                     -- decode [Closure (ByteString)] to [b]
-                    return $ map (unsafePerformIO . deserialize . decode . unClosure) clo_bs_evaluated
+                    return $ map (unsafePerformIO . deserialize) clo_bs_evaluated
                              where fromJust' :: Maybe [b] -> [b]
                                    -- just to make sure that this doesn't throw an error
                                    fromJust' Nothing = []
