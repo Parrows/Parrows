acbd4619fcbc63831095602c371d11389af295db
Martin Braun <martinbraun123@aol.com>
2017-03-09 00:38:13 +0100

    minor

--- Eden/examples/ring/test_parrows_ringSimpleRewrite.hs
+++ Eden/examples/ring/test_parrows_ringSimpleRewrite.hs
@@ -18,8 +18,6 @@ ringSimple      :: (Trans o, Trans i, Trans r) => (i -> r -> (o,r))  -- ^ ring p
                -> [i] -> [o]      -- ^ input output mapping
 ringSimple f is =  os
   where
-    (os,ringOuts)  = unzip (parMap () (toFut $ uncurry f)
-                                   (zip is $ lazy ringIns))
-    ringIns        = rightRotate ringOuts
+    (os,ringOuts)  = unzip (parMap () (toFut $ uncurry f) (zip is $ lazy $ rightRotate ringOuts))
 
 main = print $ ringSimple (\x y -> (y, x+1)) ([1..3]::[Int])
