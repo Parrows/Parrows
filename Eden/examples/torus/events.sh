94c1ff8f0f9a4fbdfb8531cb7305d6186f31dd56
Martin Braun <martinbraun123@aol.com>
2017-03-25 19:10:11 +0100

    minor

--- Eden/examples/torus/events.sh
+++ Eden/examples/torus/events.sh
@@ -2,5 +2,5 @@
 ghc torus_matrix_parrows -parmpi -rtsopts -eventlog
 ghc torus_matrix_eden -parmpi -rtsopts -eventlog
 
-./torus_matrix_parrows +RTS -N16 -ls
 ./torus_matrix_eden +RTS -N16 -ls
+./torus_matrix_parrows +RTS -N16 -ls
