33e6f312914b1f9a6011fa0719079c2035a4c0b3
Martin Braun <martinbraun123@aol.com>
2017-05-17 11:35:12 +0200

    reworking build system for the paper

---
+++ paper/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# works with:
+# texlive-base                                    install
+# texlive-binaries                                install
+# texlive-fonts-extra                             install
+# texlive-fonts-recommended                       install
+# texlive-generic-recommended                     install
+# texlive-latex-base                              install
+# texlive-latex-extra                             install
+# texlive-latex-recommended                       install
+# texlive-pictures                                install
+# texlive-science                                 install
+
+latexmk main.tex -pdf
