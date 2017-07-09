522d348c50e4b75d197f5df5056308431baef9c2
s4ke <martinbraun123@aol.com>
2017-07-09 16:51:17 +0200

    minor

--- Multicore/src/main/Parrows/Multicore.hs
+++ Multicore/src/main/Parrows/Multicore.hs
@@ -43,7 +43,8 @@ data Conf a = Conf (Strategy a)
 instance (NFData b, ArrowApply arr, ArrowChoice arr) => ArrowParallel arr a b (Conf b) where
     parEvalN (Conf strat) fs =
         listApp fs >>>
-        arr (withStrategy (parList strat))
+        arr (withStrategy (parList strat)) &&& arr id >>>
+        arr snd
 
 instance (NFData b, ArrowApply arr, ArrowChoice arr) => ArrowParallel arr a b () where
     parEvalN _ fs = parEvalN (hack fs) fs
