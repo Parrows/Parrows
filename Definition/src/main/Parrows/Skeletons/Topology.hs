a345e69a171ecc9e42776206b8e2a60ee7f1d469
Martin Braun <martinbraun123@aol.com>
2017-07-11 19:23:35 +0200

    mnor

--- Definition/src/main/Parrows/Skeletons/Topology.hs
+++ Definition/src/main/Parrows/Skeletons/Topology.hs
@@ -38,13 +38,13 @@ import Parrows.Skeletons.Map
 -- http://www.mathematik.uni-marburg.de/~eden/paper/edenCEFP.pdf
 
 (|>>>|) :: (ArrowLoop arr, ArrowChoice arr,
-            ArrowParallel arr (fut (([a], [b]), [c])) (fut (([a], [b]), [c])) (),
+            FutureEval arr (fut (([a], [b]), [c])) (fut (([a], [b]), [c])) (),
             Future fut (([a], [b]), [c])) =>
             arr a b -> arr b c -> arr a c
 (|>>>|) = pipe2 ()
 
 pipe2 :: (ArrowLoop arr, ArrowChoice arr,
-            ArrowParallel arr (fut (([a], [b]), [c])) (fut (([a], [b]), [c])) conf,
+            FutureEval arr (fut (([a], [b]), [c])) (fut (([a], [b]), [c])) conf,
             Future fut (([a], [b]), [c])) =>
             conf -> arr a b -> arr b c -> arr a c
 pipe2 conf f g =
@@ -56,17 +56,17 @@ pipe2 conf f g =
         unify :: (ArrowChoice arr) => arr a b -> arr b c -> arr (([a], [b]), [c]) (([a], [b]), [c])
         unify f g = (mapArr f *** mapArr g) *** arr (const []) >>> arr (\((a, b), c) -> ((c, a), b))
 
-pipe :: (ArrowLoop arr, ArrowParallel arr (fut a) (fut a) conf, Future fut a) => conf -> [arr a a] -> arr a a
+pipe :: (ArrowLoop arr, FutureEval arr (fut a) (fut a) conf, Future fut a) => conf -> [arr a a] -> arr a a
 pipe conf fs = unliftFut (pipeSimple conf (map liftFut fs))
 
-pipeSimple :: (ArrowLoop arr, ArrowParallel arr a a conf) => conf -> [arr a a] -> arr a a
+pipeSimple :: (ArrowLoop arr, FutureEval arr a a conf) => conf -> [arr a a] -> arr a a
 pipeSimple conf fs =
     loop (arr snd &&&
-        (arr (uncurry (:) >>> lazy) >>> parEvalN conf fs)) >>>
+        (arr (uncurry (:) >>> lazy) >>> evalN conf fs)) >>>
     arr last
 
 ring :: (ArrowLoop arr, FutureEval arr (i, fut r) (o, fut r) conf,
-    Future fut r, ArrowParallel arr (i, fut r) (o, fut r) conf) =>
+    Future fut r) =>
     conf -> arr (i, r) (o, r) -> arr [i] [o]
 ring conf f =
     loop (second (rightRotate >>> lazy) >>>
@@ -77,7 +77,6 @@ ring conf f =
 --TODO: check whether this exchanges the futures the same way as Eden does it
 torus :: (ArrowLoop arr, ArrowChoice arr, ArrowApply arr,
             FutureEval arr (c, fut a, fut b) (d, fut a, fut b) conf,
-            ArrowParallel arr (c, fut a, fut b) (d, fut a, fut b) conf,
             Future fut a, Future fut b) =>
          conf -> arr (c, a, b) (d, a, b) -> arr [[c]] [[d]]
 torus conf f =
