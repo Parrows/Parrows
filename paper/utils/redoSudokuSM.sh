42635ea83e882d1d2c214541c65d6f393be9d93f
s4ke <martinbraun123@aol.com>
2017-11-21 15:07:23 +0100

    all the sudoku benchmarks consolidated now

---
+++ paper/utils/redoSudokuSM.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+#!/usr/bin/env bash
+
+./benchmarkCalculator ../raw_benches/sudoku_sm.csv bench_sudoku_sm True
+cp bench-sudoku-sm.bench.* ../content/benchmarks/sudoku-sm
+
+originalBenchmarks=(
+    "bench-sudoku-sm.bench.eden-sudoku-sudoku17.1000.txt.csv"
+    "bench-sudoku-sm.bench.eden-sudoku-sudoku17.16000.txt.csv"
+    "bench-sudoku-sm.bench.multicore-sudoku-sudoku17.1000.txt.csv"
+    "bench-sudoku-sm.bench.multicore-sudoku-sudoku17.16000.txt.csv"
+    "bench-sudoku-sm.bench.parmonad-sudoku-sudoku17.1000.txt.csv"
+    "bench-sudoku-sm.bench.parmonad-sudoku-sudoku17.16000.txt.csv"
+)
+
+parrowsBenchmarks=(
+    "bench-sudoku-sm.bench.parrows-sudoku-parmap-eden-sudoku17.1000.txt.csv"
+    "bench-sudoku-sm.bench.parrows-sudoku-parmap-eden-sudoku17.16000.txt.csv"
+    "bench-sudoku-sm.bench.parrows-sudoku-parmap-mult-sudoku17.1000.txt.csv"
+    "bench-sudoku-sm.bench.parrows-sudoku-parmap-mult-sudoku17.16000.txt.csv"
+    "bench-sudoku-sm.bench.parrows-sudoku-parmap-par-sudoku17.1000.txt.csv"
+    "bench-sudoku-sm.bench.parrows-sudoku-parmap-par-sudoku17.16000.txt.csv"
+)
+
+outFileNames=(
+    "eden-cp-1000-diff.csv"
+    "eden-cp-16000-diff.csv"
+    "mult-1000-diff.csv"
+    "mult-16000-diff.csv"
+    "par-1000-diff.csv"
+    "par-16000-diff.csv"
+)
+
+count=${#originalBenchmarks[@]}
+
+for i in $(seq 0 $(expr ${count} - 1));
+do
+    ./calculateDifferences  ${originalBenchmarks[i]} ${parrowsBenchmarks[i]} ${outFileNames[i]}
+    cp ${outFileNames[i]} ../content/benchmarks/sudoku-sm
+done
+
+rm *.csv
